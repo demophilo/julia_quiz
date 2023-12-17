1853172305bd805a2aee6b60edb0cf9090978617
Karl Linek <karl@linek.at>
2023-12-17 20:06:19 +0100

    every capital of the worlt io

--- julia_quiz.jl
+++ julia_quiz.jl
@@ -1,1 +1,19 @@
 
+using HTTP
+using JSON
+
+function fetch_capital_cities()
+    response = HTTP.get("https://restcountries.com/v3.1/all")
+    data = JSON.parse(String(response.body))
+
+    capitals = filter(x -> haskey(x, "capital"), data)
+    capital_cities = [country["capital"] for country in capitals]
+
+    return capital_cities
+end
+
+capitals = fetch_capital_cities()
+
+# Gib die ersten 10 Hauptstädte aus
+println(capitals)
+
