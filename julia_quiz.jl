15825b8db184ac5ec77c2b5b22b3d0c54e0f0071
Karl Linek <karl@linek.at>
2023-12-26 12:28:58 +0100

    German letters

--- julia_quiz.jl
+++ julia_quiz.jl
@@ -2,6 +2,8 @@
 using HTTP
 using JSON
 
+possible_letters = Set(['a':'z'..., 'A':'Z'..., 'Ä', 'Ö', 'Ü', 'ä', 'ö', 'ü', 'ß'])
+
 function fetch_capital_cities()
     response = HTTP.get("https://restcountries.com/v3.1/all")
     data = JSON.parse(String(response.body))
@@ -18,26 +20,25 @@ function get_random_item(array)
 end
 
 function is_german_letter(letter::Char)
-    return 'a' <= letter <= 'z' || 'A' <= letter <= 'Z' || letter in 'ÄÖÜäöüß'
+    return letter in possible_letters
 end
 
-function input_letter()
-    while is_german_letter(letter) == false && length(letter) != 1
-        letter = NaN
-        println("Input single letter:")
-        letter = readline() 
-    end
-    return letter
-end
+
 
 
 capitals = fetch_capital_cities()
 
 word_to_guess = get_random_item(capitals)
 
+
+guessed_letters = Set()
+characters_to_guess = Set(collect(word_to_guess))
+right_characters = intersect(characters_to_guess, guessed_letters)
+wrong_characters = setdiff(guessed_letters, characters_to_guess)
+
 println(word_to_guess)
 
-characters_to_guess = Set(collect(word_to_guess))
+
 guessed_character = readline()
 println(guessed_character)
 println(characters_to_guess)
